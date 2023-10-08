462f3544643d6bba70ad9744d9f84a40bcb5d9c6
Nurullah Cevik <nurullah_cevik1989@hotmail.com>
2023-10-08 23:24:36 +0200

    Veritabanı ayarları güncellendi

--- MyTailorShop/settings.py
+++ MyTailorShop/settings.py
@@ -75,17 +75,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'MyTailorShop.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
     }
-}
-
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
