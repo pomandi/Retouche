08faca14e0fd56b59a14d19fbcef59e727d800b6
Nurullah Cevik <nurullah_cevik1989@hotmail.com>
2023-10-08 22:04:49 +0200

    print ozelligi eklendi

--- tailoring/admin.py
+++ tailoring/admin.py
@@ -6,21 +6,45 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import requests
+from reportlab.lib.pagesizes import A4
+from reportlab.lib import colors
+from reportlab.lib.units import cm
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+from django.http import FileResponse, HttpResponse  
+import os
+
+from io import BytesIO
 
 
 class CustomerAdmin(admin.ModelAdmin):
     change_form_template = 'admin/tailoring/customer_change_form.html'
 
-    list_display = ('name', 'wedding_date', 'location', 'is_pickup', 'email', 'phone')
-    fields = ('name', 'wedding_date', 'location', 'is_pickup', 'email', 'phone', 'email_content', 'sms_content','land','straat','huisnummer','bus','postcode','stad','productvoorraadnummer','jasmaat','vestmaat','broekmaat','services', 'description')  # yeni alanlar eklendi
-    
+    list_display = ('service_type','name', 'wedding_date', 'location', 'is_pickup', 'email', 'phone')
+    fields = ('unique_id','service_type','name', 'wedding_date', 'location', 'is_pickup', 'email', 'phone', 'email_content', 'sms_content','land','straat','huisnummer','bus','postcode','stad','productvoorraadnummer','jasmaat','vestmaat','broekmaat','services', 'description')  # yeni alanlar eklendi
+    readonly_fields = ('unique_id',) 
     def get_urls(self):
         urls = super().get_urls()
         my_urls = [
             path('<int:object_id>/send_email/', self.admin_site.admin_view(self.send_email), name='tailoring_customer_send_email'),
             path('<int:object_id>/send_sms/', self.admin_site.admin_view(self.send_sms), name='tailoring_customer_send_sms'),
+            path('<int:object_id>/print_pdf/', self.admin_site.admin_view(self.print_pdf), name='tailoring_customer_print_pdf'),  # yeni ekledik
+
         ]
         return my_urls + urls
+    
+    def print_pdf(self, request, object_id):
+            customer = Customer.objects.get(pk=object_id)
+            pdf_data = generate_pdf(customer)
+            self.message_user(request, f"PDF {object_id} numaralı müşteri için oluşturuldu.")
+            
+            response = FileResponse(BytesIO(pdf_data), content_type='application/pdf')
+            response['Content-Disposition'] = f'attachment; filename="{customer.name}.pdf"'
+            return response
+
 
 
 
@@ -100,3 +124,69 @@ class CustomerAdmin(admin.ModelAdmin):
 admin.site.register(Customer, CustomerAdmin)
 admin.site.register(TailoringService)
 admin.site.register(Location)
+
+
+def generate_pdf(customer):
+        buffer = BytesIO()
+        doc = SimpleDocTemplate(buffer, pagesize=A4)
+        
+        # PDF içeriği
+        services = ", ".join([str(service) for service in customer.services.all()])
+        location = str(customer.location) if customer.location else "N/A"
+    
+    # PDF içeriği
+        data = [
+            ["Müşteri Bilgileri", ""],
+            ["Adı", customer.name],
+            ["E-posta", customer.email],
+            ["Telefon", customer.phone],
+            ["Ülke", customer.land],
+            ["Sokak", customer.straat],
+            ["Ev Numarası", customer.huisnummer],
+            ["Otobüs Numarası", customer.bus],
+            ["Posta Kodu", customer.postcode],
+            ["Şehir", customer.stad],
+            ["Ürün Stok Numarası", customer.productvoorraadnummer],
+            ["Ceket Bedeni", customer.jasmaat],
+            ["Yelek Bedeni", customer.vestmaat],
+            ["Pantolon Bedeni", customer.broekmaat],
+            ["Düğün Tarihi", customer.wedding_date],
+            ["Teslim Alındı mı?", customer.is_pickup],
+            ["E-posta İçeriği", customer.email_content],
+            ["SMS İçeriği", customer.sms_content],
+            ["Açıklama", customer.description],
+            ["Hizmetler", services],
+            ["Konum", location]
+        ]
+        # Tablo stilini ayarlayın
+        style = TableStyle([
+            ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+            ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+            ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+            ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+            ('FONTSIZE', (0, 0), (-1, 0), 14),
+            ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+            ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
+            ('GRID', (0, 0), (-1, -1), 1, colors.black)
+        ])
+
+        # Tabloyu oluşturun
+        table = Table(data)
+        table.setStyle(style)
+
+        # Tablo boyutunu ayarlayın
+        table._argW[0] = 9 * cm
+        table._argW[1] = 9 * cm
+
+            # PDF'e ekleyin
+        elements = []
+        styles = getSampleStyleSheet()
+        title_style = styles["Heading1"]
+        title_style.alignment = 1  # Merkeze hizala
+        elements.append(Paragraph("Müşteri Bilgileri", title_style))
+        elements.append(table)
+
+        doc.build(elements)
+        pdf = buffer.getvalue()
+        buffer.close()
+        return pdf
